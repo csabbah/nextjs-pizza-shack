dc2ceb2a16f29c08224aec36749f11762d3e9879
Csabbah <csabbah@ryerson.ca>
2022-08-19 19:17:53 -0400

    Single pizza page - Price dynamically updates based on chosen options, additionally, pizza size, chosen extras & quantity are all tracked via state objects

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -4,7 +4,36 @@ import { useState } from 'react';
 import axios from 'axios';
 
 const Product = ({ pizza }) => {
-  const [size, setSize] = useState(0);
+  // Remember, pizza.prices is an array of numbers
+  const [price, setPrice] = useState(pizza.prices[0]);
+  // Initial extras are an empty array
+  const [chosenExtras, setChosenExtras] = useState([]);
+  // Initial quantity is set to 1
+  const [quantity, setQuantity] = useState(1);
+
+  // This handles the chosen size of pizza (each size of the pizza is a different al)
+  const handleSize = (sizeIndex) => {
+    setPrice(pizza.prices[sizeIndex]);
+  };
+
+  // This function handles the extras options
+  // Adding/subtracting from price and adding/removing chosen options to an array
+  const handleExtras = (e, option) => {
+    const checked = e.target.checked;
+    if (checked) {
+      // If extra is checked, add it to the active price value
+      setPrice(price + option.price);
+
+      // Add the chosen extras to the state array
+      setChosenExtras([...chosenExtras, option]);
+    } else {
+      // If extra is unchecked, minus it from the active price value
+      setPrice(price - option.price);
+
+      // Remove the extra that was unchecked
+      setChosenExtras(chosenExtras.filter((extra) => extra._id !== option._id));
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -15,19 +44,19 @@ const Product = ({ pizza }) => {
       </div>
       <div className={styles.right}>
         <div className={styles.title}>{pizza.title}</div>
-        <span className={styles.price}>${pizza.prices[size]}</span>
+        <span className={styles.price}>${price}</span>
         <div className={styles.desc}>{pizza.desc}</div>
         <h3 className={styles.choose}>Choose your size:</h3>
         <div className={styles.sizes}>
-          <div onClick={() => setSize(0)} className={styles.size}>
+          <div onClick={() => handleSize(0)} className={styles.size}>
             <Image src="/img/size.png " layout="fill" alt="" />
             <span className={styles.sizeLabel}>Small</span>
           </div>
-          <div onClick={() => setSize(1)} className={styles.size}>
+          <div onClick={() => handleSize(1)} className={styles.size}>
             <Image src="/img/size.png " layout="fill" alt="" />
             <span className={styles.sizeLabel}>Medium</span>
           </div>
-          <div onClick={() => setSize(2)} className={styles.size}>
+          <div onClick={() => handleSize(2)} className={styles.size}>
             <Image src="/img/size.png " layout="fill" alt="" />
             <span className={styles.sizeLabel}>Large</span>
           </div>
@@ -38,6 +67,7 @@ const Product = ({ pizza }) => {
             return (
               <div key={option._id} className={styles.option}>
                 <input
+                  onChange={(e) => handleExtras(e, option)}
                   type="checkbox"
                   id={option.text}
                   name={option.text}
@@ -49,7 +79,12 @@ const Product = ({ pizza }) => {
           })}
         </div>
         <div className={styles.add}>
-          <input type="number" defaultValue={1} className={styles.quantity} />
+          <input
+            onChange={(e) => setQuantity(e.target.value)}
+            type="number"
+            defaultValue={1}
+            className={styles.quantity}
+          />
           <button className={styles.button}>Add to Cart</button>
         </div>
       </div>
