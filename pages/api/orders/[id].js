8af81bf9597bf22c67778cef03fcd5af2b7cb94d
Csabbah <csabbah@ryerson.ca>
2022-08-21 15:52:19 -0400

    Order page now successfully renders the correct Order details (after purchase has been made)

--- pages/api/orders/[id].js
+++ pages/api/orders/[id].js
@@ -2,13 +2,25 @@ import dbConnect from '../../../utils/mongo';
 import Order from '../../../models/Order';
 
 const handler = async (req, res) => {
+  // Establish DB connection
+  dbConnect();
+
+  // Extract request method, query returns id 'orders/"203faangwoeiwecas"'
   const {
     method,
     query: { id },
   } = req;
 
-  if (method == 'GET') {
+  if (method === 'GET') {
+    try {
+      // --------- Method 1
+      const order = await Order.findById(id);
+      res.status(200).json(order);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
+
   if (method == 'PUT') {
   }
   if (method == 'DELETE') {
