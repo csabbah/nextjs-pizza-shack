69ced87b5c9bc7ef35c4a2704f01030c1c12160b
Csabbah <csabbah@ryerson.ca>
2022-08-22 18:43:34 -0400

    Updated Admin page CSS and Admins can now delete pizzas from the database

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../../styles/Admin.module.css';
 import Image from 'next/image';
+import axios from 'axios';
 
-const index = () => {
+const Index = ({ orders, products }) => {
+  const [pizzaList, setPizzaList] = useState(products);
+  const [orderList, setOrderList] = useState(products);
+
+  const handleDelete = async (id) => {
+    try {
+      const res = await axios.delete(
+        `http://localhost:3000/api/products/${id}`
+      );
+
+      // Normally, when we delete something from the database, it doesn't show right away
+      // So, we update the pizzaList useState object which reflects realtime changes
+      setPizzaList(pizzaList.filter((product) => product._id !== id));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log(products);
+
   return (
     <div className={styles.container}>
       <div className={styles.item}>
@@ -17,27 +37,35 @@ const index = () => {
               <th>Action</th>
             </tr>
           </tbody>
-          <tbody>
-            <tr className={styles.trTitle}>
-              <td>
-                <Image
-                  src="/img/pizza.png"
-                  width={50}
-                  height={50}
-                  objectFit="cover"
-                  alt=""
-                />
-              </td>
-              <td>PizzaId</td>
-              <td>Pizza Title</td>
-              <td>PizzaTitle</td>
-              <td>$50</td>
-              <td>
-                <button className={styles.button}>Edit</button>
-                <button className={styles.button}>Delete</button>
-              </td>
-            </tr>
-          </tbody>
+          {pizzaList.map((product) => {
+            return (
+              <tbody>
+                <tr key={product._id} className={styles.trTitle}>
+                  <td>
+                    <Image
+                      src={product.img}
+                      width={50}
+                      height={50}
+                      objectFit="cover"
+                      alt=""
+                    />
+                  </td>
+                  <td>{product._id.slice(0, 5)}...</td>
+                  <td>Pizza Title</td>
+                  <td>${product.prices[0]}</td>
+                  <td>
+                    <button className={styles.button}>Edit</button>
+                    <button
+                      className={styles.button}
+                      onClick={() => handleDelete(product._id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            );
+          })}
         </table>
       </div>
       <div className={styles.item}>
@@ -55,13 +83,13 @@ const index = () => {
           </tbody>
           <tbody>
             <tr className={styles.trTitle}>
-              <td>2315353285923151</td>
+              <td>{'2315353285923151'.slice(0, 5)}...</td>
               <td>John Doe</td>
               <td>$50</td>
               <td>paid</td>
               <td>preparing</td>
               <td>
-                <button className={styles.button}>Next Stage</button>
+                <button className={styles.nextStageBtn}>Next Stage</button>
               </td>
             </tr>
           </tbody>
@@ -71,4 +99,17 @@ const index = () => {
   );
 };
 
-export default index;
+export default Index;
+
+// Fetch all the active products and Orders via admin dashboard
+export const getServerSideProps = async () => {
+  const products = await axios.get(`http://localhost:3000/api/products`);
+  const orders = await axios.get(`http://localhost:3000/api/orders`);
+
+  return {
+    props: {
+      orders: orders.data,
+      products: products.data,
+    },
+  };
+};
